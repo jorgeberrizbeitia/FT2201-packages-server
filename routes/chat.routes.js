b026b8f70cf8572c85b7323d438067e5f0ee5064
Jorge Berrizbeitia <58472035+jorgeberrizbeitia@users.noreply.github.com>
2022-03-22 13:16:28 +0100

    socket done

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -7,11 +7,48 @@ const User = require("../models/User.model")
 router.get("/users", async (req, res, next) => {
   try {
     const response = await User.find().select("name")
-    console.log(response)
     res.status(200).json(response)
   } catch(err) {
     next(err)
   }
+})
+
+router.post("/start/:userId", async (req, res, next) => {
+
+  const { _id } = req.payload // usuario 1
+  const { userId } = req.params // usario 2
+
+  // checkear si el chat entre los dos usuarios ya existe
+  
+  try {
+    const foundChat = await Chat.findOne( { participants: { $all: [ _id, userId ] } } )
+
+    if (foundChat) {
+      res.json(foundChat)
+    } else {
+      const newChat = await Chat.create( { participants: [ _id, userId ] } )
+      res.json(newChat)
+    }
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+router.get("/messages/:chatId", async (req, res, next) => {
+
+  const { chatId } = req.params
+
+  try {
+
+    const response = await Message.find({ chatId })
+    res.json(response)
+
+  } catch(err) {
+    next(err)
+  }
+
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,48 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
+const myServer = app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
+
+
+const { Server } = require("socket.io")
+const socketioJwt = require("socketio-jwt")
+const Message = require("./models/Message.model")
+
+
+// cors
+const io = new Server (myServer, {
+  cors: {
+    origin: process.env.ORIGIN || "http://localhost:3000"
+  }
+})
+
+//token auth
+io.use(socketioJwt.authorize({
+  secret: process.env.TOKEN_SECRET,
+  handshake: true
+}))
+
+// aqui es donde iniciamos lo que escucha nuestro server de socket io
+io.on("connection", (socket) => {
+  const user = socket.decoded_token
+  console.log("Usuario conectandose " + user.name)
+
+  socket.on("join_chat", (chatId) => {
+    socket.join(chatId) // crea un room de socket con ese id y agrega al usuario
+    console.log(`usuario: ${user.name} entrado al room: ${chatId}`)
+  })
+
+  socket.on("send_message", async (messageObj) => {
+
+    const fullMessage = {...messageObj, sender: user}
+    await Message.create(fullMessage)
+    // aqui es donde emitimos los cambios a todos los usuarios del socket
+
+    socket.to(fullMessage.chatId).emit("receive_message", fullMessage) 
+    // esto envia el mensaje a todos los usuario en el socket room menos a quien lo envia
+
+    socket.emit("receive_message", fullMessage) // esto envia el mensaje de vuelta al usuario
+  })
+})
